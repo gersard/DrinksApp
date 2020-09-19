47a9e0caee31d9a291284ea3cdaa3c62df151649
Gersard <gerardo.mascayano@gmail.com>
2020-09-19 20:50:59 -0300

    - setup splash

--- app/src/main/java/cl/gerardomascayano/drinksapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/cl/gerardomascayano/drinksapp/ui/splash/SplashActivity.kt
@@ -17,11 +17,11 @@ import kotlinx.coroutines.launch
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
 //        insertInitialData()
 
         startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
     private fun insertInitialData() {
